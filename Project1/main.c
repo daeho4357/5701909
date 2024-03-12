280427e2dcef48b256ce799ab0aff171944c2812
대호 양 <yangdaeho2002@gmail.com>
2024-03-12 10:36:31 +0900

    0부터 100소수

--- Project1/main.c
+++ Project1/main.c
@@ -2,38 +2,38 @@
 
 int main() {
 
-    int sum = 0;
+    int sum=0;
 
-    printf("1에서 100까지의 소수의 합: %d\n", sum);
+    for (int i = 1; i <= 100; i++) {
+
+        int ss = 1;
+
+        if (i == 1){
+            ss = 0;
+        }
+        else
+        {
+            for (int x = 2; x < i; x++) {
+                if (i % x == 0) {
+                    ss = 0;
+                }
+            }
+        }
+
+        if (ss == 1) {
+            sum += i;
+        }
+        
+    }
+
+
+
+
+
+    printf("0부터 100까지의 소수의 합: %d\n", sum);
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
